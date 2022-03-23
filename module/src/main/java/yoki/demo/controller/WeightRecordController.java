b924e03a59e4cd8b0d46bf747272c28bff660212
Yoki Yang <yoki.yang@ingenico.com>
2022-03-23 00:21:25 -0400

    20220323 Fix the fetch API bug and deployment issue

--- module/src/main/java/yoki/demo/controller/WeightRecordController.java
+++ module/src/main/java/yoki/demo/controller/WeightRecordController.java
@@ -5,6 +5,7 @@ package yoki.demo.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class WeightRecordController {
      * @throws IOException
      */
     @RequestMapping(value = "/login")
-    boolean login(@RequestParam("userToken") String userToken) throws IOException {
+    boolean login(@RequestBody String userToken) throws IOException {
         System.out.println("LOg in");
         switch(userToken) {
             case "xinran0210":
